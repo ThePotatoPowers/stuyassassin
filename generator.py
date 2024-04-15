9e5f7062e187e35a9a65fdf2e452f84f4ad391d2
Zidane Karim <zkarim7676@gmail.com>
2024-04-15 19:18:07 -0400

    updated for 4/15

--- generator.py
+++ generator.py
@@ -8,98 +8,103 @@ names = names_file.read().split('\n')
 
 names_file.close()
 
+while True:
+    choice = input("""Welcome! Here are your options:\n
 
-choice = input("""Welcome! Here are your options:\n
+    - new: Generate a new list of pairs
+    - update: Update the list after a kill
+    - reset: Reset the list
+    - view: View the current list
+    - exit: Exit the program\n
 
-- new: Generate a new list of pairs
-- update: Update the list after a kill
-- reset: Reset the list
-- view: View the current list
-- exit: Exit the program\n
+    """)
 
-""")
-
-if choice == 'new':
-    print('Generating new list...')
-    targets = []
-    pairs = []
-
-    for name in names:
-        target = random.choice(names)
-        # make sure the target is not the same as the name and that the target is not already in the list
-        # also make sure the target and the name are not EACH OTHER's targets
-
-        while target == name or target in targets or (name in targets and target == pairs[targets.index(name)]['target']):
-            target = random.choice(names)
-
-
-
-        targets.append(target)
-
-
-        pair = {'name': name, 'target': target}
-        pairs.append(pair)
-
-    with open('pairs.csv', 'w', newline='') as csvfile:
-        fieldnames = ['name', 'target']
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-
-        writer.writeheader()
-        for pair in pairs:
-            writer.writerow(pair)
-
-    print('Pairs generated and saved to pairs.csv')   
-
-elif choice == 'update':
-    killer = input("Got it! Who got a kill? (name):\n")
-    with open('pairs.csv', 'r') as csvfile:
-        reader = csv.DictReader(csvfile)
+    if choice == 'new':
+        print('Generating new list...')
+        targets = []
         pairs = []
-        for row in reader:
-            pairs.append(row)
-        
-        for pair in pairs:
-            if pair['name'] == killer:
-                target = pair['target']
-                break
-        
-        for pair in pairs:
-            if pair['name'] == target:
-                new_target = pair['target']
-                break
-        
-        for pair in pairs:
-            if pair['name'] == killer:
-                pair['target'] = new_target
-            if pair['name'] == target:
-                # remove the pair from the list
-                pairs.remove(pair)
-        
-        with open('pairs.csv', 'w', newline='') as csvfile:
+
+        for name in names:
+            target = random.choice(names)
+            # make sure the target is not the same as the name and that the target is not already in the list
+            # also make sure the target and the name are not EACH OTHER's targets
+
+            while target == name or target in targets or (name in targets and target == pairs[targets.index(name)]['target']):
+                target = random.choice(names)
+
+
+
+            targets.append(target)
+
+
+            pair = {'name': name, 'target': target}
+            pairs.append(pair)
+
+        with open('public/pairs.csv', 'w', newline='') as csvfile:
             fieldnames = ['name', 'target']
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
             writer.writeheader()
             for pair in pairs:
                 writer.writerow(pair)
+
+        print('Pairs generated and saved to pairs.csv')   
+
+    elif choice == 'update' or choice == 'kill':
+        killer = input("Got it! Who got a kill? (name):\n")
+        with open('public/pairs.csv', 'r') as csvfile:
+            reader = csv.DictReader(csvfile)
+            pairs = []
+            for row in reader:
+                pairs.append(row)
+
+            target = ''
+            new_target = ''
             
-        print(f'{killer} has killed {target}! New target for {killer} is {new_target}')
+            for pair in pairs:
+                if pair['name'] == killer:
+                    target = pair['target']
+                    print(f'Target for {killer} is {target}')
+                    break
+            
+            for pair in pairs:
+                if pair['name'] == target:
+                    new_target = pair['target']
+                    print(f'Target for {target} is {new_target}')
+                    break
+            
+            for pair in pairs:
+                if pair['name'] == killer:
+                    pair['target'] = new_target
+                if pair['name'] == target:
+                    # remove the pair from the list
+                    pairs.remove(pair)
+            
+            with open('public/pairs.csv', 'w', newline='') as csvfile:
+                fieldnames = ['name', 'target']
+                writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
-elif choice == 'exit':
-    print('Goodbye!')
-    exit()
+                writer.writeheader()
+                for pair in pairs:
+                    writer.writerow(pair)
+                
+            print(f'{killer} has killed {target}! New target for {killer} is {new_target}')
 
-elif choice == 'reset':
-    with open('pairs.csv', 'w', newline='') as csvfile:
-        fieldnames = ['name', 'target']
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    elif choice == 'exit':
+        print('Goodbye!')
+        exit()
 
-        writer.writeheader()
+    elif choice == 'reset':
+        with open('public/pairs.csv', 'w', newline='') as csvfile:
+            fieldnames = ['name', 'target']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
-    print('Pairs list has been reset!')
+            writer.writeheader()
 
-elif choice == 'view':
-    with open('pairs.csv', 'r') as csvfile:
-        reader = csv.DictReader(csvfile)
-        for row in reader:
-            print(row)
+        print('Pairs list has been reset!')
+
+    elif choice == 'view':
+        with open('public/pairs.csv', 'r') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                print(row)
